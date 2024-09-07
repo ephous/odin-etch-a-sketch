4b96e8e0b79ad01e0295a572feeffb0bc91c6e5e
ephous <179286206+ephous@users.noreply.github.com>
2024-09-07 13:53:29 -0400

    Add button to resize the grid with user-provided values and improve div sizing so that fixed widths and heights are not needed.

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const container = document.querySelector('#container');
-for (let i=0;i<16;i++){
-    
-    let gridRow = document.createElement('div');
+const container = document.querySelector("#container");
+
+function createNewGrid(n) {
+  
+  for (let i = 0; i < n; i++) {
+    let gridRow = document.createElement("div");
     gridRow.className = "grid-row";
     container.appendChild(gridRow);
-    
-    for (let i=0;i<16;i++){
-        let gridSquare = document.createElement('div');
-        gridSquare.className = "grid-square";
-        gridRow.appendChild(gridSquare);
+
+    for (let i = 0; i < n; i++) {
+      let gridSquare = document.createElement("div");
+      gridSquare.className = "grid-square";
+      gridRow.appendChild(gridSquare);
     }
+  }
+
+  container.addEventListener(
+    "mouseover",
+    (e) => {
+      //works too: e.target.style.backgroundColor="rebeccapurple";
+      if (e.target.className == "grid-square") {
+        e.target.className = "grid-square marked";
+      }
+    },
+    false
+  );
 }
 
-container.addEventListener('mouseover', (e) => {
-    //works too: e.target.style.backgroundColor="rebeccapurple";
-    if(e.target.className=="grid-square"){
-        e.target.className="grid-square marked";
-    }
-}, false)
+function requestNewGrid() {
+  const answer = prompt("How many squares per size (limit 100)?", 16);
+  if (answer === null) return;
+  if (!Number.isInteger(Number(answer))) return;
+  Array.from(container.children).forEach(
+    x => container.removeChild(x) );
+  createNewGrid( Math.min(100, Math.abs(answer)) );
+}
+
+// initialize
+createNewGrid(16);
